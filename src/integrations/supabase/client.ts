1f15a5ad463f72c7e7bda23d72cb99a48d0bd38b
lucasdreger <lucasdreger@gmail.com>
2025-02-05 13:51:05 +0100

    c

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import { formatCurrency } from "./formatters";
 
 const supabaseUrl = 'https://gqulqtialfsoabpqrkqs.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdxdWxxdGlhbGZzb2FicHFya3FzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgwOTY3MjIsImV4cCI6MjA1MzY3MjcyMn0.kdD-Y72BxIJYG9K_FFjfUPwrg_tUKdWCwCXID-2pD1A';
